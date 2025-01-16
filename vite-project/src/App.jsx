08c71a34a9924c4370a63c7c1062b815fb64bbfc
Timothy Ehli <tehli1882@gmail.com>
2025-01-16 18:54:02 -0500

    catching up

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Home } from './components/Home'
-import WordList from './components/WordList'
+import './App.css';
+import { Home } from './components/Home';
+import WordList from './components/WordList';
 
 function App() {
-
-
   return (
     <>
-    <Home/>
-    <WordList/>
-
-      
+      <Home />
+      <WordList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/components/WordList.jsx
+++ vite-project/src/components/WordList.jsx
@@ -1,33 +1,32 @@
+import { useEffect, useState } from 'react';
 
-import { useEffect, useState } from "react";
-
-const dictionaryUrl = 'https://1rnoszgn46.execute-api.us-east-1.amazonaws.com/multi-details';
-
+const dictionaryUrl =
+  'https://1rnoszgn46.execute-api.us-east-1.amazonaws.com/multi-details';
 
 function WordList() {
-    const [words, setWords] = useState([]);
-    const [activeButton, setActiveButton] = useState(0);
+  const [words, setWords] = useState([]);
+  const [activeButton, setActiveButton] = useState(null); // Start with null to indicate no active button
 
-    useEffect(() => {
-        fetch(dictionaryUrl)
-            .then((response) => response.json())
-            .then((data) => setWords(data.dictionaries));
-    }, []); 
+  useEffect(() => {
+    fetch(dictionaryUrl)
+      .then((response) => response.json())
+      .then((data) => setWords(data.dictionaries));
+  }, []);
 
-
-    return (
-        <>
-            <h3>Subjects</h3>
-            {words.map((word) => {
-                return (
-                    <button className={activeButton === word.id ?  "bg-black" : "bg-white" } key={word.id} onClick={activeButton} >
-                        <p>{word.title}</p>
-                    
-                    </button>
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      <h3>Subjects</h3>
+      {words.map((word) => (
+        <button
+          key={word.id}
+          className={activeButton === word.id ? 'bg-black' : 'bg-white'} // Conditional className
+          onClick={() => setActiveButton(word.id)} // Update state to make this button active
+        >
+          <p>{word.title}</p>
+        </button>
+      ))}
+    </>
+  );
 }
 
 export default WordList;
