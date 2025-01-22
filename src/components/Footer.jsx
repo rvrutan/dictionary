583db38c788fe4c62c0af0da2aa0b560849c276c
rvrutan <rutanroni@gmail.com>
2025-01-21 21:47:57 -0500

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-stone-300 p-4 ">
+      <div className="container mx-auto text-center text-stone-700 ">
+        <a href="https://github.com/Gurprasad34" className="p-20 font-bold text-lg underline"> Gurprasad Singh</a>
+        <a href="https://github.com/rvrutan" className="p-20 font-bold text-lg underline "> Roni v. Rutan</a>
+        <a href="https://github.com/Saosyn" className="p-20 font-bold text-lg underline "> Tim Ehli</a>
+      </div>
+    </footer>
+  );
+}
