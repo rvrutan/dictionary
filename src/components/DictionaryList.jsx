a714f2b766d283496dd399eb780a0f26fc1d2263
rvrutan <rutanroni@gmail.com>
2025-01-22 22:05:59 -0500

    mobile styling added

--- src/components/DictionaryList.jsx
+++ src/components/DictionaryList.jsx
@@ -15,15 +15,15 @@ function DictionaryList({ setTag }) {
   }, []);
 
   return (
-    <div>
-      <div className="button-container">
+    <div className="px-4 py-2">
+      <div className="flex flex-wrap justify-center gap-4">
         {dictionaries.map((dictionary) => (
           <button
             key={dictionary._id}
-            className="bg-indigo-600 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded-3xl mx-8 my-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+            className="bg-indigo-600 hover:bg-indigo-900 text-white font-bold py-2 px-6 rounded-3xl my-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 w-full sm:w-auto"
             onClick={() => setTag(dictionary.tags[0])}
           >
-            <p>{dictionary.title}</p>
+            <p className="text-center">{dictionary.title}</p>
           </button>
         ))}
       </div>
